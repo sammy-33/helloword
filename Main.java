002ec4fbff931c600439ff46781731ecb590937c
sammy-33 <47538750+sammy-33@users.noreply.github.com>
2023-06-24 06:17:45 +0000

    input num1 & num2

--- Main.java
+++ Main.java
@@ -1,7 +1,29 @@
+import java.util.Scanner;
+
 class Main {
   public static void main(String[] args) {
     System.out.println("Hello world!");
     System.out.println("nice to meet you.");
 
+    int a = 15;
+    int b = 0;
+    
+    System.out.println(a + a);
+    
+    Scanner scan = new Scanner(System.in);
+
+    System.out.println("num1 = ");
+    int num1 = scan.nextInt();
+    System.out.println("num2 = ");
+    int num2 = scan.nextInt();
+
+    b = num1 + num2;
+    
+    System.out.println(num1);
+    System.out.println(num2);
+
+    scan.close();
+
+    System.out.println(String.format("b = %d, b*b = %d",b,b*b));
   }
 }
