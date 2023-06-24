ccda9ae62347721fda0e943a936b92afaaecf650
sammy-33 <47538750+sammy-33@users.noreply.github.com>
2023-06-24 04:01:29 +0000

    bug fix

--- Main.java
+++ Main.java
@@ -2,10 +2,6 @@ class Main {
   public static void main(String[] args) {
     System.out.println("Hello world!");
     System.out.println("nice to meet you.");
-    
-    char parcentSign = '%';
-    
-    string formattedString = String.format("I am 100%c", parcentSign);
-    System.out.println(formattedString);
+
   }
 }
